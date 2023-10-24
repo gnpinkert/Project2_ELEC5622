eec96e790ee05df95dae0349bdcc9b2f74e6eefd
Grant <grant.pinkert@gmail.com>
2023-10-24 14:24:57 +1100

    Moved some stuff around to make more sense, added an empty plot.py file which will be used to hold plotting code. Also removed the cleaning of a directory since we just create a new directory on every run anyways

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -19,25 +19,22 @@ def main():
     training_details = TrainingDetails(batch_size=BATCH_SIZE,
                                        learning_rate=0.0004,
                                        momentum=0.9,
-                                       epochs=10,
+                                       epochs=1,
                                        output_dir=Path(""))
     network = AlexNet()
     output_directory = make_output_directory(training_details=training_details)
     training_details.output_dir = output_directory
-    try:
-        logger = create_logger(training=True, final_output_path=output_directory)
-        logger.info('using args:')
-        logger.info(args)
-        register_sig_abrt(directory_to_clean=output_directory)
-        train_net(net=network,
-                  trainloader=train_loader,
-                  valloader=validation_loader,
-                  training_details=training_details,
-                  logging=logging)
-        unregister_sig_abrt()
-    except Exception as e:
-        print(f"Caught exception {e} during training, cleaning directory {output_directory}")
-        shutil.rmtree(output_directory)
+
+    logger = create_logger(training=True, final_output_path=output_directory)
+    logger.info('using args:')
+    logger.info(args)
+    register_sig_abrt(directory_to_clean=output_directory)
+    train_net(net=network,
+              trainloader=train_loader,
+              valloader=validation_loader,
+              training_details=training_details,
+              logging=logging)
+    unregister_sig_abrt()
 
     eval_net(net=network, loader=test_loader, final_output_path=output_directory)
 
